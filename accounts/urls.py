f3e48432ad68d764801e166cec3193789ae32e09
Amritpal31 <amritpalsingh0031@gmail.com>
2021-05-06 08:29:03 +0000

    first commit

---
+++ accounts/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from django.contrib.auth.views import LoginView,LogoutView
+from . import views
+
+app_name = 'accounts'
+
+urlpatterns = [
+    url(r'^signup/$', views.SignUp.as_view(), name='signup'),
+    url(r'^login/$',
+    LoginView.as_view(template_name='accounts/login.html'),
+    name='login'),
+    url(r'^logout/$',LogoutView.as_view(),name='logout'),
+]
